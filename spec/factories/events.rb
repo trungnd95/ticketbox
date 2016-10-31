7c393d28563ccf2f7706729529a2036770233d2b
Trung Nguyen <ndt8895@gmail.com>
2016-10-31 16:43:08 +0700

    Change test

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,17 +1,17 @@
 FactoryGirl.define do
   factory :event do
     name { Faker::Name.name }
-    extended_html_description { "No need to care about that thing" }
+    extended_html_description { "Content event ..." }
     category
     venue
     starts_at { 10.days.ago }
     ends_at { 1.day.from_now}
     published { true }
-  
-    trait :in_the_past do 
+
+    trait :in_the_past do
       ends_at { 5.day.ago }
     end
-  
+
     trait :in_the_future do
       ends_at { 10.days.from_now }
     end
